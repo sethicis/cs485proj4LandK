0b48d47b2103c62bd09371c782be85166ebd9964
Kyle Blagg <noahjemkai@gmail.com>
2013-12-11 16:22:50 -0500

    Added tiny web server code and changed proxy.c to a c++ program.

---
+++ proxy.cc
@@ -0,0 +1,297 @@
+/*
+ * proxy.c - CS:APP Web proxy
+ *
+ * TEAM MEMBERS: (put your names here)
+ *     Student Kyle Blagg, kyle.blagg@uky.edu 
+ *     Student Name2, student2@cs.uky.edu 
+ * 
+ * IMPORTANT: Give a high level description of your code here. You
+ * must also provide a header comment at the beginning of each
+ * function that describes what that function does.
+ */ 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include "csapp.h"
+    
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * Function prototypes
+ */
+int parse_uri(char *uri, char *target_addr, char *path, int  *port);
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum,
+				 char *shortmsg, char *longmsg);
+
+/* 
+ * main - Main routine for the proxy program 
+ */
+int main(int argc, char **argv)
+{
+    int listenfd, connfd, port, clientlen;
+    struct sockaddr_in clientaddr;
+	
+    /* Check command line args */
+    if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+    }
+    port = atoi(argv[1]);
+	
+    listenfd = Open_listenfd(port);
+    while (1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+		doit(connfd);                                             //line:netp:tiny:doit
+		Close(connfd);                                            //line:netp:tiny:close
+    }
+}
+
+
+/*
+ * parse_uri - URI parser
+ * 
+ * Given a URI from an HTTP proxy GET request (i.e., a URL), extract
+ * the host name, path name, and port.  The memory for hostname and
+ * pathname must already be allocated and should be at least MAXLINE
+ * bytes. Return -1 if there are any problems.
+ */
+int parse_uri(char *uri, char *hostname, char *pathname, int *port)
+{
+    char *hostbegin;
+    char *hostend;
+    char *pathbegin;
+    int len;
+
+    if (strncasecmp(uri, "http://", 7) != 0) {
+        hostname[0] = '\0';
+        return -1;
+    }
+       
+    /* Extract the host name */
+    hostbegin = uri + 7;
+    hostend = strpbrk(hostbegin, " :/\r\n\0");
+    len = hostend - hostbegin;
+    strncpy(hostname, hostbegin, len);
+    hostname[len] = '\0';
+    
+    /* Extract the port number */
+    *port = 80; /* default */
+    if (*hostend == ':')   
+        *port = atoi(hostend + 1);
+    
+    /* Extract the path */
+    pathbegin = strchr(hostbegin, '/');
+    if (pathbegin == NULL) {
+        pathname[0] = '\0';
+    }
+    else {
+        pathbegin++;
+        strcpy(pathname, pathbegin);
+    }
+
+    return 0;
+}
+
+/*
+ * format_log_entry - Create a formatted log entry in logstring. 
+ * 
+ * The inputs are the socket address of the requesting client
+ * (sockaddr), the URI from the request (uri), and the size in bytes
+ * of the response from the server (size).
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, 
+		      char *uri, int size)
+{
+    time_t now;
+    char time_str[MAXLINE];
+    unsigned long host;
+    unsigned char a, b, c, d;
+
+    /* Get a formatted time string */
+    now = time(NULL);
+    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    /* 
+     * Convert the IP address in network byte order to dotted decimal
+     * form. Note that we could have used inet_ntoa, but chose not to
+     * because inet_ntoa is a Class 3 thread unsafe function that
+     * returns a pointer to a static variable (Ch 13, CS:APP).
+     */
+    host = ntohl(sockaddr->sin_addr.s_addr);
+    a = host >> 24;
+    b = (host >> 16) & 0xff;
+    c = (host >> 8) & 0xff;
+    d = host & 0xff;
+
+
+    /* Return the formatted log entry string */
+    sprintf(logstring, "%s: %d.%d.%d.%d %s", time_str, a, b, c, d, uri);
+}
+/*
+ * doit - handle one HTTP request/response transaction
+ */
+/* $begin doit */
+void doit(int fd)
+{
+    int is_static;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;
+	
+    /* Read request line and headers */
+    Rio_readinitb(&rio, fd);
+    Rio_readlineb(&rio, buf, MAXLINE);                   //line:netp:doit:readrequest
+    sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
+    if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
+		clienterror(fd, method, "501", "Not Implemented",
+					"Basic-Proxy does not implement this method");
+        return;
+    }                                                    //line:netp:doit:endrequesterr
+    read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
+	
+    /* Parse URI from GET request */
+    is_static = parse_uri(uri, filename, cgiargs);       //line:netp:doit:staticcheck
+    if (Stat(filename, &sbuf) < 0) {                     //line:netp:doit:beginnotfound
+		clienterror(fd, filename, "404", "Not found",
+					"Basic-Proxy couldn't find this file");
+		return;
+    }                                                    //line:netp:doit:endnotfound
+	
+    if (is_static) { /* Serve static content */
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //line:netp:doit:readable
+			clienterror(fd, filename, "403", "Forbidden",
+						"Basic-Proxy couldn't read the file");
+			return;
+		}
+		serve_static(fd, filename, sbuf.st_size);        //line:netp:doit:servestatic
+    }
+    else { /* Serve dynamic content */
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { //line:netp:doit:executable
+			clienterror(fd, filename, "403", "Forbidden",
+						"Tiny couldn't run the CGI program");
+			return;
+		}
+		serve_dynamic(fd, filename, cgiargs);            //line:netp:doit:servedynamic
+    }
+}
+/* $end doit */
+
+/*
+ * read_requesthdrs - read and parse HTTP request headers
+ */
+/* $begin read_requesthdrs */
+void read_requesthdrs(rio_t *rp)
+{
+    char buf[MAXLINE];
+	
+    Rio_readlineb(rp, buf, MAXLINE);
+    while(strcmp(buf, "\r\n")) {          //line:netp:readhdrs:checkterm
+		Rio_readlineb(rp, buf, MAXLINE);
+		printf("%s", buf);
+    }
+    return;
+}
+
+/*
+ * serve_static - copy a file back to the client
+ */
+/* $begin serve_static */
+void serve_static(int fd, char *filename, int filesize)
+{
+    int srcfd;
+    char *srcp, filetype[MAXLINE], buf[MAXBUF];
+	
+    /* Send response headers to client */
+    get_filetype(filename, filetype);       //line:netp:servestatic:getfiletype
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");    //line:netp:servestatic:beginserve
+    sprintf(buf, "%sServer: Basic Proxy Server\r\n", buf);
+    sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+    sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+    Rio_writen(fd, buf, strlen(buf));       //line:netp:servestatic:endserve
+	
+    /* Send response body to client */
+    srcfd = Open(filename, O_RDONLY, 0);    //line:netp:servestatic:open
+    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);//line:netp:servestatic:mmap
+    Close(srcfd);                           //line:netp:servestatic:close
+    Rio_writen(fd, srcp, filesize);         //line:netp:servestatic:write
+    Munmap(srcp, filesize);                 //line:netp:servestatic:munmap
+}
+
+/*
+ * get_filetype - derive file type from file name
+ */
+void get_filetype(char *filename, char *filetype)
+{
+    if (strstr(filename, ".html"))
+		strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif"))
+		strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".jpg"))
+		strcpy(filetype, "image/jpeg");
+    else
+		strcpy(filetype, "text/plain");
+}
+/* $end serve_static */
+
+/*
+ * serve_dynamic - run a CGI program on behalf of the client
+ */
+/* $begin serve_dynamic */
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+    char buf[MAXLINE], *emptylist[] = { NULL };
+	
+    /* Return first part of HTTP response */
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Server: Basic Proxy Server\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+	
+    if (Fork() == 0) { /* child */ //line:netp:servedynamic:fork
+		/* Real server would set all CGI vars here */
+		setenv("QUERY_STRING", cgiargs, 1); //line:netp:servedynamic:setenv
+		Dup2(fd, STDOUT_FILENO);         /* Redirect stdout to client */ //line:netp:servedynamic:dup2
+		Execve(filename, emptylist, environ); /* Run CGI program */ //line:netp:servedynamic:execve
+    }
+    Wait(NULL); /* Parent waits for and reaps child */ //line:netp:servedynamic:wait
+}
+/* $end serve_dynamic */
+
+/*
+ * clienterror - returns an error message to the client
+ */
+/* $begin clienterror */
+void clienterror(int fd, char *cause, char *errnum,
+				 char *shortmsg, char *longmsg)
+{
+    char buf[MAXLINE], body[MAXBUF];
+	
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+	
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    Rio_writen(fd, buf, strlen(buf));
+    Rio_writen(fd, body, strlen(body));
+}
+/* $end clienterror */
